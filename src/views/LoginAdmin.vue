d618c6591433167a2c14c920af24d3a37079440a
AndrésFE22 <aarteagaramos@correo.unicordoba.edu.co>
2023-12-20 19:42:23 -0500

    login

--- src/views/LoginAdmin.vue
+++ src/views/LoginAdmin.vue
@@ -1,5 +1,16 @@
 <template>
     <v-app id="inspire">
+      <v-app-bar app color="white" dense height="70">
+        <img
+        src="../assets/TraductorEMBERA.png"
+        height="60"
+        contain
+      >
+      <v-spacer></v-spacer>
+      <v-btn icon @click="openUserMenu">
+        <v-icon>mdi-home  </v-icon>
+      </v-btn>
+    </v-app-bar>
       <div class="background">
         <v-content>
         <v-container class="fill-height" fluid >
@@ -12,8 +23,8 @@
                       <v-col cols="12" md="8">
                         <v-card-text class="mt-12">
                           <h1
-                            class="text-center display-2 teal--text text--accent-3"
-                          >Sign in to Diprella</h1>
+                            class="text-center display-2 orange--text text--darken-3"
+                          >Iniciar Sesión</h1>
   
                           <v-form>
                             <span class="mdi mdi-account"></span>
@@ -21,7 +32,7 @@
                               label="Usuario"
                               name="Usuario"
                               type="text"
-                              color="teal accent-3"
+                              color="orange darken-3"
                               v-model="username"
                             />
                             <span class="mdi mdi-lock"></span>
@@ -30,21 +41,21 @@
                               label="Contraseña"
                               name="Contraseña"
                               type="password"
-                              color="teal accent-3"
-                              v-model="password"
+                              color="orange darken-3"
+                              v-model="password"  
                             />
                           </v-form>
                         </v-card-text>
                         <div class="text-center mt-3">
-                          <v-btn rounded color="teal accent-3" dark @click="signIn">SIGN IN</v-btn>
+                          <v-btn rounded color="orange darken-1" dark @click="signIn">Inciar Sesión</v-btn>
                         </div>
                       </v-col>
-                      <v-col cols="12" md="4" class="teal accent-3">
-                        <v-card-text class="white--text mt-12">
-                          <h1 class="text-center display-1">Hello, Friend!</h1>
-                          <h5
-                            class="text-center"
-                          >Enter your personal details and start journay with us</h5>
+                      <v-col cols="12" md="4" class="custom-background rounded-lg">
+                        <v-card-text class="brown--text text--darken-3 mt-16">
+                          <h1 class="text-center display-1 transparent--text">Bienvenido</h1>
+                          <h3
+                            class="text-center transparent--text"
+                          >Inicia sesión para poder administrar la base de datos</h3>
                         </v-card-text>
                       </v-col>
                     </v-row>
@@ -82,6 +93,10 @@
         console.log("Credenciales incorrectas");
       }
     },
+    openUserMenu(){
+      this.$router.push('/');
+    }
+    
   },
   };
   </script>
@@ -93,5 +108,16 @@ background-size: cover;
 height: 100vh;
 }
 
+
+.custom-background {
+  background: url('../assets/6.png') center center/cover no-repeat; /* Ajusta la ruta de la imagen */
+}
+
+@media only screen and (max-width: 768px) {
+  .custom-background {
+    background: url('../assets/5.png') center center/cover no-repeat;
+  }
+
+}
 </style>
   
